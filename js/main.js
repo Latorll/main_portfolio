0baa0cd71fb130205955a182c3f1a820f7e6c4a2
Latorll <82001849+Latorll@users.noreply.github.com>
2021-10-14 15:10:00 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,4 +23,7 @@ $(function(){
         const sector = $('body').children();
         $(sector).not('content').toggleClass('blur');
     });
+
+
+    $('body').children().css('background-position', 'center');
 });
