222f58bffd81b2aa12befa56f5c9ab83384fcdb4
Latorll <82001849+Latorll@users.noreply.github.com>
2023-02-27 21:13:25 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-$(function(){
-    var fadeIn = $('.fadein');
-    $(window).on('scroll', function () {
+const fadeIn = $('.fadein');
+$(window).on('scroll', function () {
+    // スクロールアニメーション
     $(fadeIn).each(function () {
-        var offset = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
+        const offset = $(this).offset().top;
+        const scroll = $(window).scrollTop();
+        const windowHeight = $(window).height();
         if (scroll > offset - windowHeight + 150) {
-        $(this).addClass("scroll-in");
+        $(this).addClass('scroll-in');
         }
     });
-});
-    $('.drawer-list a').click(function(){
-        $('label').trigger('click');
-        var id = $(this).attr('href');
-        var position = $(id).offset().top;
-        $('html,body').animate({
-            'scrollTop': position
-        }, 500);
-    });
 
-    $('label').click(function(){
-        const sector = $('body').children();
-        $(sector).not('content').toggleClass('blur');
-    });
+    const y = window.scrollY;
+    if(y>780){
+        $('label').addClass('white');
+    }else{
+        $('label').removeClass('white');
+    }
+});
+$('.drawer-list a').click(function(){
+    $('label').trigger('click');
+    const id = $(this).attr('href');
+    const position = $(id).offset().top;
+    $('html,body').animate({
+        'scrollTop': position
+    }, 500);
+});
+
+$('label').click(function(){
+    const sector = $('body').children();
+    $(sector).not('content').toggleClass('blur');
 });
